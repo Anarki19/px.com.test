f5f4a791a4835959362ac19974308c611a93b90a
a,aksionau <Anarki19@yandex.ru>
2025-07-08 18:22:42 +0200

    Fixed solution for better perfomance

--- px.com.test/OptimizedSolution.cs
+++ px.com.test/OptimizedSolution.cs
@@ -32,37 +32,33 @@ public class OptimizedSolution
         MapVendorsToClients(clients, vendors);
     }
 
-    // Тут идея решения простая, но мне нравится оно больше, ибо мы не проходим каждый раз для всех клиентов всех вендоров. 
-    // Клиенты и вендоры удаляются из очереди и добавляются обратно, только если остались либо денюжки у клиентов либо ресурсы
-    // Тут сложно алгоритма не линейная O(n), а O(n + m) что отработает гораздо быстрее
+    // Переделал алгорит еще раз. Теперь точно обходит клиента и вендоров один раз, в стэк возвращаю обратно только в том случае,
+    // если остались лиды
     private static List<Client> MapVendorsToClients(List<Client> clients, List<Vendor> vendors)
     {
-        // Создаем очереди только из клиентов и вендоров с ресурсами/бюджетом
-        var clientQueue = new Queue<Client>(clients.Where(c => c.Budget > 0));
-        var vendorQueue = new Queue<Vendor>(vendors.Where(v => v.Leads > 0));
+        // Создаем очереди только для вендоров.
+        var vendorStack = new Stack<Vendor>(vendors);
         
-        // Основной цикл распределения - O(n + m)
-        while (clientQueue.Count > 0 && vendorQueue.Count > 0)
+        foreach (var client in clients)
         {
-            var client = clientQueue.Dequeue();
-            var vendor = vendorQueue.Dequeue();
-            
-            // Распределяем ресурсы между текущим клиентом и вендором
-            var resourcesToAllocate = Math.Min(client.Budget, vendor.Leads);
-            
-            client.Budget -= resourcesToAllocate;
-            vendor.Leads -= resourcesToAllocate;
-            
-            // Добавляем вендора в список клиента
-            client.VendorNames.Add(vendor.Name);
-            
-            // Если у клиента остался бюджет, возвращаем его в очередь
-            if (client.Budget > 0)
-                clientQueue.Enqueue(client);
-            
-            // Если у вендора остались ресурсы, возвращаем его в очередь
-            if (vendor.Leads > 0)
-                vendorQueue.Enqueue(vendor);
+            if(client.Budget <= 0)
+                continue;
+
+            do
+            {
+                var vendor = vendorStack.Pop();
+                var resourcesToAllocate = Math.Min(client.Budget, vendor.Leads);
+
+                client.Budget -= resourcesToAllocate;
+                vendor.Leads -= resourcesToAllocate;
+                
+                client.VendorNames.Add(vendor.Name);
+                
+                if(vendor.Leads > 0)
+                    vendorStack.Push(vendor);
+
+
+            } while (client.Budget > 0 && vendorStack.Count > 0);
         }
 
         return clients;
